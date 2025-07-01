fc14de36f1b6ddf414adbdf26b7f4987d87a3a9c
Shurgbee <81319757+shurgbee@users.noreply.github.com>
2025-07-01 10:48:19 -0500

    Initial Commit

---
+++ src/app/component/header.tsx
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCat } from "@fortawesome/free-solid-svg-icons/faCat";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+
+export default function Header() {
+  return (
+    <div className="flex flex-row w-screen bg-blue-600 min-h-9vh text-6xl items-center p-10vh text-white justify-between gap-5">
+        <a href="/" className="p-2">
+            <FontAwesomeIcon icon={faCat}/>
+        </a>
+        <a href="/" className="">
+            <FontAwesomeIcon icon={faShoppingCart} className=""/>
+        </a>
+    </div>
+  );
+}
